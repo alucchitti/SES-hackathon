06175e63452195a0a32e7f27e888bf875e4bfe0c
adp21 <andrewpomeroy02@gmail.com>
2021-08-12 15:57:43 -0400

    fsfsdfsdf

--- src/App.js
+++ src/App.js
@@ -2,27 +2,14 @@ import "./App.css";
 import Cookies from "js-cookie";
 import Login from "./login/login.js";
 import Main from "./overview/Main.js";
-import axios from "axios";
-
-var apiKey = '1cb35cfe6eeba07ad5afa33e1e997d12'
-var baseUrl = 'http://api.nessieisreal.com'
-
 function App() {
   // Uncomment this code to see graph dashboard 
   //   return (
   //     <Main />
   //   )
-  
-
   var id = Cookies.get('accountId')
   if (id) {
-    axios.get(`${baseUrl}/customers/${id}?key=${apiKey}`).then((response) => {
-      console.log(response.data);
-      }).catch((err, response) => {
-          Cookies.remove('accountId');
-          window.location.reload();
-      })
-      return <Main />;
+      return <Main id={id}/>;
   } else {
     return <Login />;
   }
--- src/overview/Main.js
+++ src/overview/Main.js
@@ -1,14 +1,40 @@
 import "../App.css";
+import Cookies from "js-cookie";
 import "./Main.css";
 /* Chartjs Examples: https://reactchartjs.github.io/react-chartjs-2/#/ */
 import LineChart from "../line/Line.js";
+import React from 'react';
 import PieChart from "../pie/Pie.js";
+import axios from "axios";
 
-const Main = () => {
-  return (
-    <div id="Main">
+var apiKey = '1cb35cfe6eeba07ad5afa33e1e997d12'
+var baseUrl = 'http://api.nessieisreal.com'
+
+class Main extends React.Component {
+  constructor(props){
+    super(props);
+    this.id = props.id
+    this.state = {
+      firstName: null,
+      lastName: null
+    }
+  }
+  componentDidMount(){
+    axios.get(`${baseUrl}/customers/${this.id}?key=${apiKey}`).then((response) => {
+      console.log(response.data);
+      this.setState({firstName: response.data.first_name, lastName: response.data.last_name});
+      }).catch((err, response) => {
+        console.log(err);
+        alert("error getting data")
+          Cookies.remove('accountId');
+          window.location.reload();
+    })
+  }
+  render(){
+    return(
+      <div id="Main">
       <div>
-        <p id="header">Budget Overview</p>
+        <p id="header">Welcome back {this.state.firstName} {this.state.lastName} (UID: {this.id})</p>
       </div>
       <hr id="spacer" />
       <div className="center-wrapper">
@@ -18,8 +44,9 @@ const Main = () => {
         <div className="center">
         </div>
       </div>
-    </div>
-  );
+      </div>
+    );
+  }
 };
 
 export default Main;
