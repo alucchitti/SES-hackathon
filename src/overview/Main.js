53f905676a2087e1364a8a54dde5d7bbad835487
mna1021 <76842973+mna1021@users.noreply.github.com>
2021-08-12 10:42:58 -0400

    fixed css for charts
    charts were transparent and stacked on each other

--- src/overview/Main.js
+++ src/overview/Main.js
@@ -1,28 +1,26 @@
-import '../App.css';
-import './Main.css';
+import "../App.css";
+import "./Main.css";
 /* Chartjs Examples: https://reactchartjs.github.io/react-chartjs-2/#/ */
-import LineChart from '../line/Line.js';
-import PieChart from '../pie/Pie.js';
+import LineChart from "../line/Line.js";
+import PieChart from "../pie/Pie.js";
 
 const Main = () => {
-    return (
-        <div id='Main'>
-            <div id='header'>
-                <h1>Budget Overview</h1>
-            </div>
-            <hr/>
-            <div className="center-wrapper">
-                <div className="center">
-                    <LineChart/>
-                </div>
-            </div>
-            <div className="center-wrapper">
-                <div className="center">
-                    <PieChart/>
-                </div>
-            </div>
+  return (
+    <div id="Main">
+      <div>
+        <p id="header">Budget Overview</p>
+      </div>
+      <hr id="spacer" />
+      <div className="center-wrapper">
+        <div className="center">
+          <LineChart />
         </div>
-    )
-}
+        <div className="center">
+          <PieChart />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Main
+export default Main;
