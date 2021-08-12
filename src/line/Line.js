b909d50a07ce6291c24e31e363978ed7a380a414
Melanie Chan <melanie.chan@sjsu.edu>
2021-08-12 08:41:59 -0700

    made line & pie charts into functions
    to connect line & pie chart

--- src/line/Line.js
+++ src/line/Line.js
@@ -2,39 +2,42 @@ import './Line.css';
 import { Line } from 'react-chartjs-2';
 import PieChart from '../pie/Pie.js';
 
-const data = {
-    labels: ['1', '2', '3', '4', '5', '6'],
-    datasets: [
-      {
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        fill: false,
-        backgroundColor: 'rgb(0, 58, 111)',
-        borderColor: 'rgb(0, 58, 111, .2)',
-      },
-    ],
-};
-
-const options = {
-    scales: {
-      yAxes: [
+function LineChart() {
+  const data = {
+      labels: ['1', '2', '3', '4', '5', '6'],
+      datasets: [
         {
-          ticks: {
-            beginAtZero: true,
-          },
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          fill: false,
+          backgroundColor: 'rgb(0, 58, 111)',
+          borderColor: 'rgb(0, 58, 111, .2)',
         },
       ],
-    },
+  };
 
-    /* when a dot is clicked on , show new pie chart*/
-    onClick: (e) => {
-      alert("change UI to show new pie");
-    }
+  const options = {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
 
-};
+      /* when a dot is clicked on , show new pie chart*/
+      onClick: (e) => {
+        alert("change UI to show new pie");
+        console.log(e);
+      }
 
-const LineChart = () => (
-    <Line data={data} options={options}/>
-);
+  };
+
+  return (
+      <Line data={data} options={options}/>
+  );
+}
 
 export default LineChart;
