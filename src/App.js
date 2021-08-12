41f93c0bf2dee48813cc06d2f833158eb06ce877
alucchitti <ael74@pitt.edu>
2021-08-12 09:41:34 -0400

    Add login styling and footer with logo

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import Login from "./login/login.js";
 import Main from "./overview/Main.js";
 
 function App() {
-  /* Uncomment this code to see graph dashboard */
-   return (
-     <Main />
-   )
-  /*
+  // /* Uncomment this code to see graph dashboard */
+  //  return (
+  //    <Main />
+  //  )
+  // 
   var id = Cookies.get('accountId')
   console.log(id);
   if (id) {
@@ -16,7 +16,7 @@ function App() {
   } else {
     return <Login />;
   }
-  */
+  
 }
 
 export default App;
