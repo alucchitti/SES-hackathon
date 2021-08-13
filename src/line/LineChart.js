8920960649b2abe30b25f2bedae98f8ebd42c819
Melanie Chan <melanie.chan@sjsu.edu>
2021-08-12 18:58:27 -0700

    added pie chart frontend

--- src/line/LineChart.js
+++ src/line/LineChart.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types'
 import { Line } from 'react-chartjs-2';
 //import * as ChartAnnotation from 'chartjs-plugin-annotation';
+import PieChart from '../pie/Pie.js';
 
 const budgetLimit = 110;
 
@@ -12,49 +13,16 @@ const periodSelections = [
   'yearly', 'monthly', 'weekly', 'daily?'
 ]
 
-// Chart.options (refer to chart.js) used for LineChart component constructor
-const options = {
-  scales: {
-    yAxes: [
-      {
-        gridLines: [{
-          drawTicks: true,
-          display: true
-        }]
-      },
-    ],
-    xAxes: [{
-      type: 'time',
-      position: 'bottom',
-      time: {
-        displayFormats: { 'day': 'MM/YY' },
-        tooltipFormat: 'MM/DD/YY',
-        unit: 'month',
-      }
-    }],
-  },
-  annotation: {
-    annotations: [{
-      type: 'line',
-      mode: 'horizontal',
-      scaleID: 'y-axis-0',
-      value: budgetLimit,
-      borderColor: 'rgb(75, 192, 192)',
-      borderWidth: 4,
-      label: {
-        enabled: true,
-        content: 'Budget Limit'
-      }
-    }]
-  }
-}
 
 class LineChart extends React.Component {
+
   constructor(props) {
     super(props);
 
-    this.graphOptions = options;
     this.graphData = props;
+    this.state = {
+      pieChartValues: null, // pie chart values reflect data of current dot on line graph
+    }
 
     // idk if we need state?
   }
@@ -83,10 +51,43 @@ class LineChart extends React.Component {
       }]
     };
 
+    // Chart.options (refer to chart.js) used for LineChart component constructor
+    let options = {
+      scales: {
+        yAxes: [
+          {
+            gridLines: [{
+              drawTicks: true,
+              display: true
+            }]
+          },
+        ],
+        xAxes: [{
+          type: 'time',
+          position: 'bottom',
+          time: {
+            displayFormats: { 'day': 'MM/YY' },
+            tooltipFormat: 'MM/DD/YY',
+            unit: 'month',
+          }
+        }],
+      },
+
+      /* when a dot is clicked on , show new pie chart*/
+      onClick: (e) => {
+        console.log(e);
+          this.setState({ pieChartValues: [e.x, e.y]}) // pie values depend on current point's x and y
+      }
+    }
+
 
     return (
       <div className="line-chart-container">
-        <Line data={ldata} options={this.graphOptions} />
+        <Line data={ldata} options={options} />
+
+        {console.log(this.state.pieChartValues)}
+        { /* condition ? what gets displayed when condition true : when condition false */
+          this.state.pieChartValues !== null ? <PieChart budgetValues={this.state.pieChartValues}/> : <></> }
       </div>
     );
   }
